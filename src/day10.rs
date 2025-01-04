cb85951609dffc507255c296bfd4f678ff49147c
Kyle Guarco <kyleguarco55@gmail.com>
2025-01-04 16:43:04 -0500

    Added the rest of 2024 boilerplate

---
+++ src/day10.rs
@@ -0,0 +1,31 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+#[aoc_generator(day10)]
+fn parse(_input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day10, part1)]
+fn part1(_input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day10, part2)]
+fn part2(_input: &str) -> String {
+    todo!()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+}
